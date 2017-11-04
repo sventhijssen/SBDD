b83fbf5df9855dcf73c5be4fc7ca25bb9a496c49
Semen <semen.cher@mail.ru>
2017-11-04 16:56:18 +0700

    Реализовал базовые операции.

--- SBDD/sbdd.cpp
+++ SBDD/sbdd.cpp
@@ -1,1 +1,50 @@
 #include "sbdd.h"
+
+namespace sbdd {
+
+	SBDD::SBDD(bool constant)
+	{
+		nextNum_ = 2;
+		TableStr tableStr;
+		tableStr.index = tableStr.left = tableStr.right = -1;
+		table_.insert(std::pair<int, TableStr>(constant ? 1 : 0, tableStr));
+	}
+
+	SBDD::SBDD(const SBDD &sbdd)
+	{
+		nextNum_ = sbdd.nextNum_;
+		table_ = sbdd.table_;
+	}
+
+	bool SBDD::member(const TableStr &tableStr)
+	{
+		auto iter = table_.begin();
+		auto end = table_.end();
+		for (; iter != end; ++iter) {
+			if (iter->second == tableStr) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	int SBDD::lookup(const TableStr &tableStr)
+	{
+		auto iter = table_.begin();
+		auto end = table_.end();
+		for (; iter != end; ++iter) {
+			if (iter->second == tableStr) {
+				return iter->first;
+			}
+		}
+		return -1;
+	}
+
+	int SBDD::insert(const TableStr &tableStr)
+	{
+		table_.insert(std::pair<int, TableStr>(nextNum_, tableStr));
+		nextNum_++;
+		return nextNum_ - 1;
+	}
+
+}
--- SBDD/sbdd.h
+++ SBDD/sbdd.h
@@ -1,9 +1,45 @@
 #pragma once
 
-class SBDD
-{
-public:
+#include <map>
 
-private:
+namespace sbdd {
 
-};	// class SBDD
+	// —труктура дл€ представлени€ строчки таблицы.
+	struct TableStr {
+		int index;
+		int left;
+		int right;
+
+		bool operator == (const TableStr &tableStr)
+		{
+			if (index == tableStr.index && left == tableStr.left && right == tableStr.right) {
+				return true;
+			}
+			else
+				return false;
+		}
+	};
+
+	class SBDD
+	{
+	public:
+		// ≈сли constant = false, то будет создан SBDD = 0, иначе 1.
+		SBDD(bool constant = false);
+		SBDD(const SBDD &sbdd);
+
+		// Ѕазовые операции.
+		// —уществует ли данные узел в таблице.
+		bool member(const TableStr &tableStr);
+		// ¬озвращает номер узла в таблице, если такого узла нет то -1.
+		int lookup(const TableStr &tableStr);
+		// ¬ставл€ет узел в таблицу и возвращает его номер.
+		int insert(const TableStr &tableStr);
+
+
+	private:
+		std::map<int, TableStr> table_;
+		int nextNum_;
+
+	};	// class SBDD
+
+}
