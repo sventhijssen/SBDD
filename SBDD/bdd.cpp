ee0eb44e5e80b251ab43d484619f1c5a7ed4d1a0
Semen <semen.cher@mail.ru>
2017-12-28 14:50:19 +0700

    Альтернативный вариант.

--- SBDD/bdd.cpp
+++ SBDD/bdd.cpp
@@ -113,12 +113,24 @@ int BDD::applyPrivate(BDD &result, BDD &bdd2, OP op, int u1, int u2)
 			}
 			else {
 				if (table_[u1].index < bdd2.table_[u2].index) {
-					u = result.makeNode(bdd2.table_[u2].index, applyPrivate(result, bdd2, op, u1, bdd2.table_[u2].left),
-						applyPrivate(result, bdd2, op, u1, bdd2.table_[u2].right));
+					if (table_[u1].index != -1) {
+						u = result.makeNode(table_[u1].index, applyPrivate(result, bdd2, op, u1, bdd2.table_[u2].left),
+							applyPrivate(result, bdd2, op, u1, bdd2.table_[u2].right));
+					}
+					else {
+						u = result.makeNode(bdd2.table_[u2].index, applyPrivate(result, bdd2, op, u1, bdd2.table_[u2].left),
+							applyPrivate(result, bdd2, op, u1, bdd2.table_[u2].right));
+					}
 				}
 				else {
-					u = result.makeNode(table_[u1].index, applyPrivate(result, bdd2, op, table_[u1].left, u2),
-						applyPrivate(result, bdd2, op, table_[u1].right, u2));
+					if (bdd2.table_[u2].index != -1) {
+						u = result.makeNode(bdd2.table_[u2].index, applyPrivate(result, bdd2, op, table_[u1].left, u2),
+							applyPrivate(result, bdd2, op, table_[u1].right, u2));
+					}
+					else {
+						u = result.makeNode(table_[u1].index, applyPrivate(result, bdd2, op, table_[u1].left, u2),
+							applyPrivate(result, bdd2, op, table_[u1].right, u2));
+					}
 				}
 			}
 		}
